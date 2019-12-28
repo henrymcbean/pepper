29d2b7919b1b86c52fdaf375329ed1722384a5cd
henrymcbean <henry_mcbean@hotmail.com>
2019-12-28 19:31:48 +0000

    A more complex join, adding key values.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -10,9 +10,13 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'pepper';
+  features$: AngularFireList<any[]>;
   cuisines$: AngularFireList<any[]>;
   restaurant$: AngularFireList<any[]>;
+
+  feature: Observable<any[]>;
   cuisines: Observable<any[]>;
+  features: Observable<any[]>;
   restaurants: Observable<any[]>;
 
   constructor(private db: AngularFireDatabase) {
@@ -20,16 +24,32 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.cuisines$ = this.db.list('/cuisines');
+    this.features$ = this.db.list('/features');
     this.restaurant$ = this.db.list('/restaurants');
 
     this.cuisines = this.cuisines$.valueChanges();
+    this.features = this.features$.valueChanges();
+
     this.restaurants = this.restaurant$.valueChanges()
       .pipe(map(restaurants => {
-        console.log('BEFORE MAP', restaurants);
         restaurants.map(restaurant => {
-          restaurant.cuisineType = this.db.object('/cuisines/' + restaurant.cuisine).valueChanges();
+          restaurant.featureTypes = [];
+          // tslint:disable-next-line: forin
+          for (let f in restaurant.features) {
+            const featureName = '/features/' + f;
+            this.feature = this.db.object(featureName).snapshotChanges()
+              .subscribe(action => {
+                console.log('Type', action.type);
+                // console.log('Key', action.key);
+                // console.log('Value', action.payload.val());
+                restaurant.featureTypes.push({ key: action.key, value: action.payload.val() });
+                console.log('restaurant', restaurant);
+              });
+
+            //console.log(this.feature);
+            console.log(restaurant);
+          }
         });
-        console.log('AFTER MAP', restaurants);
         return restaurants;
       }));
   }
